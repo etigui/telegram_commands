f1bc7112c0895247835ad34d16a0dbe917204c84
etigui <32940684+etigui@users.noreply.github.com>
2018-10-30 10:16:42 +0100

    Opti

--- src/qtrezbemwmxwzrqunpsyipfejqjg05bw.php
+++ src/qtrezbemwmxwzrqunpsyipfejqjg05bw.php
@@ -3,13 +3,13 @@ ini_set('error_reporting', E_ALL);
 
 // Set url to send back response message
 $bot_token = "744205053:AAH9GZH1gwNQ_7XZTj_GegpnT4H_ir75fpk;
-$website = "https://api.telegram.org/bot".$bot_token;
+$website = "https://api.telegram.org/bot{$bot_token}";
 
 // Get message contenant
 $update = file_get_contents('php://input');
 $update = json_decode($update, TRUE);
 
-// Get commands and user id
+// Get commands and user chat id
 $chat_id = $update["message"]["chat"]["id"];
 $message = $update["message"]["text"];
 
@@ -28,7 +28,8 @@ switch($message){
 
 // Send message back to user
 function send_message($chat_id, $message){
-	$ur1 = $GLOBALS['website']."/sendMessage?chat_id=".$chat_id."&text=".urlencode($message);
-	file_get_contents($ur1);
+	$data = ['chat_id' => $chat_id,'text' => urlencode($message)];
+	$url = "{$GLOBALS['website']}/sendMessage?".http_build_query($data);
+	file_get_contents($ur1);	
 }
 ?>

6cfc8e795ab66ebc8cc4355dea9130d9fcbd582d
etigui <32940684+etigui@users.noreply.github.com>
2018-11-04 11:30:14 +0100

    Create send_photo.php

---
+++ src/send_photo.php
@@ -0,0 +1,25 @@
+<?php
+// Telegram var
+$caption = urlencode("Photo to send !!!");
+$chat_id = "000000000";
+$token = "744205053:AAH9GZH1gwNQ_7XZTj_GegpnT4H_ir75fpk";
+// File var
+$file_name = "file.txt";
+$file_path = "./directory/{$file_name}";
+$mime_type = mime_content_type($file_path);
+// Send file
+$curl = curl_init();
+curl_setopt_array($curl, [
+    CURLOPT_URL => "https://api.telegram.org/bot{$token}/sendPhoto?caption={$caption}&chat_id={$chat_id}",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HTTPHEADER => [
+        'Content-Type: multipart/form-data'
+    ],
+    CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => [
+        'photo' => curl_file_create($file_path, $mime_type , $file_name)
+    ]
+]);
+$data = curl_exec($curl);
+curl_close($curl);
+?>
